972a9061e4bd81dd8bafb919309334f3d0af13bb
lubin <whitneylubin@icloud.com>
2019-02-28 19:46:45 -0500

    firebase logs submit info

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCKEJ9xzcuSE8P5ZfQxTOqm89Vsb6zDqQE",
+    authDomain: "prettypennylanding.firebaseapp.com",
+    databaseURL: "https://prettypennylanding.firebaseio.com",
+    projectId: "prettypennylanding",
+    storageBucket: "prettypennylanding.appspot.com",
+    messagingSenderId: "198086476877"
+  };
+  firebase.initializeApp(config);
+
+
+//Reference companies collection
+
+var companiesRef = firebase.database().ref('companies');
+
+
+
 // Listen for form submit
 
 document.getElementById('contactForm').addEventListener('submit', submitForm);
 
+
+// Submit Form
 function submitForm(e){
     e.preventDefault();
 
@@ -13,7 +33,10 @@ var email = getInputVal('email');
 var phone = getInputVal('phone');
 var company = getInputVal('company');
 
-console.log(firstname);
+// Save Company
+
+saveCompany(firstname, lastname, email, phone, company);
+
 
 }
 
@@ -21,4 +44,16 @@ console.log(firstname);
 
 function getInputVal(id) {
     return document.getElementById(id).value;
+}
+
+// Save companies to firebase
+function saveCompany(firstname, lastname, company, email, phone){
+    var newCompanyRef = companiesRef.push();
+    newCompanyRef.set({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        phone: phone,
+        company:company
+    });
 }
