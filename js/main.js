c98c1299972c07b978c88146e0258ceee92135e9
Whitney <39642663+whitneylubin@users.noreply.github.com>
2025-03-18 04:17:34 +0000

    update enviroment varibales

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+require('dotenv').config(); // Load .env variables
+
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCKEJ9xzcuSE8P5ZfQxTOqm89Vsb6zDqQE",
-    authDomain: "prettypennylanding.firebaseapp.com",
-    databaseURL: "https://prettypennylanding.firebaseio.com",
-    projectId: "prettypennylanding",
-    storageBucket: "prettypennylanding.appspot.com",
-    messagingSenderId: "198086476877"
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
-  firebase.initializeApp(config);
+  
+  firebase.initializeApp(firebaseConfig);
+  
 
 
 //Reference companies collection
